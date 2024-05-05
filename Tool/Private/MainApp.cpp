4fde9ea855dcd5b1e3d1ae4f198e087a43acbf37
none <106803681+gitdainn@users.noreply.github.com>
2024-05-05 11:59:05 +0900

    ImGui 기초 완료

---
+++ Tool/Private/MainApp.cpp
@@ -0,0 +1,169 @@
+#include "stdafx.h"
+
+#pragma region IMGUI
+// 다른 헤더보다 먼저 추가해야 오류 안 뜸
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#pragma endregino
+
+#include "../Public/MainApp.h"
+#include "GameInstance.h"
+
+USING(Tool)
+
+CMainApp::CMainApp()
+	: m_pGameInstance(CGameInstance::GetInstance())
+{
+	Safe_AddRef(m_pGameInstance);
+}
+
+HRESULT CMainApp::Initialize()
+{
+	GRAPHIC_DESC			GraphicDesc;
+	ZeroMemory(&GraphicDesc, sizeof GraphicDesc);
+
+	GraphicDesc.eWinMode = GRAPHIC_DESC::MODE_WIN;
+	GraphicDesc.hWnd = g_hWnd;
+	GraphicDesc.iViewSizeX = g_iWinSizeX;
+	GraphicDesc.iViewSizeY = g_iWinSizeY;
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	if (FAILED(m_pGameInstance->Initialize_Engine(LEVEL_END, g_hInst, GraphicDesc, &m_pDevice, &m_pContext)))
+		return E_FAIL;
+
+	if (FAILED(Ready_Prototype_Component_For_Static()))
+		return E_FAIL;
+
+	if (FAILED(Ready_Prototype_GameObject_For_Static()))
+		return E_FAIL;
+
+	if (FAILED(Ready_Prototype_Sprite_For_Static()))
+		return E_FAIL;
+
+	if (FAILED(SetUp_StartLevel(LEVEL_LOGO)))
+		return E_FAIL;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(g_hWnd);
+	ImGui_ImplDX11_Init(m_pDevice, m_pContext);
+
+	return S_OK;
+}
+
+void CMainApp::Tick(_double TimeDelta)
+{
+	m_pGameInstance->Tick_Engine(TimeDelta);
+
+
+#ifdef _DEBUG
+	m_TimeAcc += TimeDelta;
+#endif // _DEBUG
+}
+
+HRESULT CMainApp::Render()
+{
+	ImGui_ImplDX11_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	static bool bIsDemo = true;
+	ImGui::ShowDemoWindow(&bIsDemo);
+
+	ImGui::Render();
+
+	m_pGameInstance->Clear_BackBuffer_View(_float4(0.f, 0.f, 1.f, 1.f));
+	m_pGameInstance->Clear_DepthStencil_View();
+
+	m_pRenderer->Draw_RenderGroup();
+
+#ifdef _DEBUG
+	++m_dwNumDraw;
+
+	if (m_TimeAcc >= 1.0)
+	{
+		wsprintf(m_szFPS, TEXT("fps : %d"), m_dwNumDraw);
+		m_TimeAcc = 0.0;
+		m_dwNumDraw = 0;
+	}
+#endif
+
+	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+	m_pGameInstance->Present();
+
+	return S_OK;
+}
+
+HRESULT CMainApp::SetUp_StartLevel(LEVEL eNextLevelID)
+{
+	//return m_pGameInstance->Open_Level(LEVEL_LOADING, CLevel_Loading::Create(m_pDevice, m_pContext, eNextLevelID));
+	return S_OK;
+}
+
+HRESULT CMainApp::Ready_Prototype_Component_For_Static()
+{
+	/* For.Prototype_Component_Renderer */
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Renderer"),
+		m_pRenderer = CRenderer::Create(m_pDevice, m_pContext))))
+		return E_FAIL;
+
+	/* For.Prototype_Component_Transform*/
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Transform"),
+		CTransform::Create(m_pDevice, m_pContext))))
+		return E_FAIL;
+
+	///* For.Prototype_Component_VIBuffer_Rect */
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_VIBuffer_Rect"),
+	//	CVIBuffer_Rect::Create(m_pDevice, m_pContext))))
+	//	return E_FAIL;
+
+	///* For.Prototype_Component_Shader_VtxTex*/
+	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_STATIC, TEXT("Prototype_Component_Shader_VtxTex"),
+	//	CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Shader_VtxTex.hlsl"), VTXTEX_DECLARATION::Elements, VTXTEX_DECLARATION::iNumElements))))
+	//	return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CMainApp::Ready_Prototype_GameObject_For_Static()
+{
+	return S_OK;
+}
+
+HRESULT CMainApp::Ready_Prototype_Sprite_For_Static()
+{
+	return S_OK;
+}
+
+CMainApp* CMainApp::Create()
+{
+	CMainApp* pInstance = new CMainApp();
+
+	if (FAILED(pInstance->Initialize()))
+	{
+		MSG_BOX("Failed to Created CMainApp");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CMainApp::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pContext);
+	Safe_Release(m_pDevice);
+	Safe_Release(m_pGameInstance);
+
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	CGameInstance::Release_Engine();
+}
+
